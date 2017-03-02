d16da9fb6b857a2830c020fe55212d765ddaa949
YAEL KFIR <yael.kfir6@gmail.com>
2017-03-02 15:15:26 +0200

    ne js for tests

---
+++ js/tetsts.js
@@ -0,0 +1,15 @@
+
+const classes = ['one', 'two'];
+
+function createClasses(classesArr) {
+
+  let classesStr = '';
+
+  classesArr.forEach((e) => { classesStr += ' '+ e;});
+
+  return arrConected
+
+}
+
+createClasses(classes);
+console.info(createClasses(classes));
